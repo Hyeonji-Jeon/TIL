a24a731da196dd8f1698cb6f2557583f64a76457
Hyeonji Jeon <guswl1397@naver.com>
2025-02-13 18:10:26 +0900

    0213

---
+++ 02.13/0213/src/main/java/org/zerock/controller/LoginController.java
@@ -0,0 +1,51 @@
+package org.zerock.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginController
+ */
+@WebServlet("/login")
+public class LoginController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//jakarta.servlet.http.Cookie
+		Cookie loginCookie = new Cookie("login", "AAA");
+		loginCookie.setMaxAge(60*60*24);
+		loginCookie.setPath("/");
+		
+		//응답 보낼때 response에 추가해 주어야 함 
+		response.addCookie(loginCookie);
+		
+		
+		
+	}
+
+}
