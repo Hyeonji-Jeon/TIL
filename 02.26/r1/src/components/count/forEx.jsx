7a538091ce391fad73f194b728f52edef4646e64
Hyeonji Jeon <guswl1397@naver.com>
2025-02-26 16:46:32 +0900

    0226

---
+++ 02.26/r1/src/components/count/forEx.jsx
@@ -0,0 +1,33 @@
+
+
+function ForEx() {
+
+    //배열을 map을 이용해서 JSX로 바꾸는 것 - 동적으로 생성되는 JSX
+    //React 처리시에 모든 JSX는 고유한 ID가 생성됨 - 내부적으로 사용
+    //동적으로 하다보면 ID를 생성할 수 없으므로 개발자가 직접 ID를 부여
+    //이러한 ID를 key라고 부른다
+
+    const arr = [
+        {no:1, name:'AAA', price: 3000},
+        {no:2, name:'BAA', price: 4000},
+        {no:3, name:'CAA', price: 5000},
+        {no:4, name:'DAA', price: 6000},
+    ]
+
+    return (
+        <div>
+            <ul>
+                {arr.map( ({no,name,price}) =>
+                    <li key={no}>
+                        {name} -- {price}
+                    </li>
+                )}
+            </ul>
+            <ul>
+                {arr.map( (m,idx) => <li key={idx}> {m.name} -- {m.price} </li>)}
+            </ul>
+        </div>
+    );
+}
+
+export default ForEx;
---
+++ 02.26/r1/src/components/todo/todoAdd.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+
+function TodoAdd() {
+
+    async function handleClick () {
+
+        const data = {title:'React Todo Test',writer:'jhj'}
+
+        const res = await axios.post('http://122.34.51.94:8090/api/v1/todos', data)
+
+        console.log(res)
+    }
+
+    return (
+        <div>
+            <button onClick={handleClick}>SEND</button>
+        </div>
+    );
+}
+
+export default TodoAdd;
