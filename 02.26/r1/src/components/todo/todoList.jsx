dd5402e0cd70a66380ddc0890eb4d3e0f62c9334
Hyeonji-Jeon <guswl1397@naver.com>
2025-03-04 11:59:37 +0900

    Update todoList.jsx

--- 02.26/r1/src/components/todo/todoList.jsx
+++ 02.26/r1/src/components/todo/todoList.jsx
@@ -4,7 +4,7 @@ function TodoList() {
 
     const handleClick = async () => {
 
-        const res = await axios.get('http://122.34.51.94:8090/api/v1/todos');
+        const res = await axios.get('http://localhost:8090/api/v1/todos');
 
         console.log(res)
     }
