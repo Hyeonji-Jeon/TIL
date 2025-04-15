ba99b740a13993faae3a9091c6a8fd518687ebab
Hyeonji Jeon <guswl1397@naver.com>
2025-04-15 16:45:58 +0900

    0415

---
+++ 4월/04.15/rr1/app/components/todo/listComponent.tsx
@@ -0,0 +1,34 @@
+import {useQuery} from "@tanstack/react-query";
+import {testTodoList} from "~/api/todoAPI";
+import {useSearchParams} from "react-router";
+
+
+function TodoListComponent (){
+
+    const [searchParams] = useSearchParams();
+
+    const pageStr = searchParams.get("page") || "1"
+    const sizeStr = searchParams.get("size") || "10"
+
+    console.log("pageStr: ", pageStr," sizeStr: ", sizeStr)
+
+    const query = useQuery({
+        queryKey: ['todos'],
+        queryFn: () => testTodoList(pageStr, sizeStr),
+        staleTime: 10 * 60 * 1000,
+    })
+
+    const {isFetching, data, error} = query
+
+    return(
+        <div>
+            <div className={'text-4xl'}>Todo List Component</div>
+            <div className={'text-3xl bg-amber-600'}>{isFetching && <h1>Loading..........</h1>}</div>
+
+            {data && <div className={'text-2xl'}>List 출력</div>}
+
+        </div>
+    )
+}
+
+export default TodoListComponent
---
+++ 4월/04.15/sb2/src/main/java/org/zerock/sb2/config/CustomSecurityConfig.java
@@ -0,0 +1,64 @@
+package org.zerock.sb2.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@Configuration
+@EnableMethodSecurity(prePostEnabled = true)
+public class CustomSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        log.info("-------------configure-------");
+        http.cors(cors -> {
+            cors.configurationSource(corsConfigurationSource());
+        });
+
+        //로그인 화면 필요 없음
+        http.formLogin(config -> {
+           config.disable();
+        });
+
+        http.csrf(config -> config.disable());
+
+        //세션 생성 필요 없음
+        http.sessionManagement(config -> {
+            config.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
+        return http.build();
+    }
+
+    //CORS 설정
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        corsConfiguration.setAllowedOriginPatterns(List.of("*")); // 어디서든 허락
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+
+
+}
